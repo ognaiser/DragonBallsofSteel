42a575e4eb58a2005e3745b9c8d93ede2b5e745c
Manuel Pedro <mbpedro1997@gmail.com>
2017-06-18 00:54:18 +0100

    Skin Updates

--- src/org/academia/dragonballsofsteel/SkinTypeExtra.java
+++ src/org/academia/dragonballsofsteel/SkinTypeExtra.java
@@ -1,8 +1,12 @@
 package org.academia.dragonballsofsteel;
 
-public enum  SkinTypeExtra {
-    Back("Resources/Background_DB.jpg"),
-    BackCity("Resources/backcity.png");
+public enum SkinTypeExtra {
+    Back("Resources/DB_Screens/Background_DB.jpg"),
+    BackCity("Resources/DB_Screens/backcity.png"),
+    Start_Screen("Resources/DB_Screens/DB_StartScreen_1.png"),
+    Start_Screen_2("Resources/DB_Screens/DB_StartScreen_2.png"),
+    Vegeta_Wins("Resources/DB_Screens/DB_Vegeta_Wins_Screen.png"),
+    Goku_Wins("Resources/DB_Screens/DB_Goku_Wins_Screen.png");
 
     private String path;
 
--- src/org/academia/main/SkinMenus.java
+++ src/org/academia/main/SkinMenus.java
@@ -2,7 +2,7 @@ package org.academia.main;
 
 public enum SkinMenus {
     ArcadeMenu("Resources/Arcade_Menu.png"),
-    DragonBalls("Resources/dragon_ball_menu.png");
+    DragonBalls("Resources/DB_Screens/dragon_ball_menu.png");
 
 
     private String path;
