dda7202ef2ecbc5fa3a98b14b34204d97aa100ec
Manuel Pedro <mbpedro1997@gmail.com>
2017-06-18 01:44:11 +0100

    Easter Egg Started

--- src/org/academia/main/SkinMenus.java
+++ src/org/academia/main/SkinMenus.java
@@ -2,7 +2,9 @@ package org.academia.main;
 
 public enum SkinMenus {
     ArcadeMenu("Resources/Arcade_Menu.png"),
-    DragonBalls("Resources/DB_Screens/dragon_ball_menu.png");
+    DragonBalls("Resources/DB_Screens/dragon_ball_menu.png"),
+    SnipeMenu("Resources/sniper_elite_menu.jpg");
+
 
 
     private String path;
